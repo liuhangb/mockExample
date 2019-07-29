2b417c0c8fafbe2d667a783f84fb506f2f4ee56d
liuhang <1542951820@qq.com>
2019-07-29 16:26:17 +0800

    【需求】 配置线上环境

--- mock/index.js
+++ mock/index.js
@@ -11,6 +11,7 @@ const mocks = [
 // please use it cautiously, it will redefine XMLHttpRequest,
 // which will cause many of your third-party libraries to be invalidated(like progress event).
 export function mockXHR() {
+    console.log("mockXHR")
     // mock patch
     // https://github.com/nuysoft/Mock/issues/300
     Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
